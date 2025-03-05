ad99e9b9b103e8db0ff19c252298f9ca382e5a50
Ahmed <ahmed.g@turing.com>
2025-03-05 15:52:17 +0100

    Add Dependency Injection and TP3 Description

--- app/src/main/java/com/example/afya/presentation/viewmodel/AddPostViewModel.kt
+++ app/src/main/java/com/example/afya/presentation/viewmodel/AddPostViewModel.kt
@@ -1,12 +1,13 @@
 package com.example.afya.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.afya.domain.usecase.AddPostUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 
 @HiltViewModel
-class AddProductViewModel @Inject constructor() : ViewModel() {
+class AddPostViewModel @Inject constructor(private val addPostUseCase: AddPostUseCase) : ViewModel() {
 
     //TODO: complete you logic
 }
